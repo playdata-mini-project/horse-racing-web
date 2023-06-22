e87ab6f95e77982691c020bbe99d3ca8837241c4
leejmyeong <zdlwoaud@naver.com>
2023-06-22 12:49:02 +0900

    jm13

---
+++ src/main/java/com/naver/servlet/GameServlet.java
@@ -0,0 +1,99 @@
+package com.naver.servlet;
+
+import com.naver.domain.entity.game.Game;
+import com.naver.domain.entity.horse.Horse;
+import com.naver.domain.entity.horse.Horses;
+import com.naver.util.SplitUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/game")
+public class GameServlet extends HttpServlet {
+
+    private Game game;
+
+    @Override
+    public void init() throws ServletException {
+        game = new Game();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>Race Result</title></head>");
+        out.println("<body>");
+        out.println("<h1>Race Result</h1>");
+        out.println("<table>");
+        out.println("<tr>");
+        out.println("<th>말 이름</th>");
+        out.println("<th>유저</th>");
+        out.println("<th>도달 거리</th>");
+        out.println("</tr>");
+        for (Horse horse : game.getHorses().getHorses()) {
+            out.println("<tr>");
+            out.println("<td>" + horse.getName() + "</td>");
+            out.println("<td>" + horse.getUser() + "</td>");
+            out.println("<td>" + horse.getDistance().getValue() + "</td>");
+            out.println("</tr>");
+        }
+        out.println("</table>");
+
+        Horse winningHorse = game.getWinningHorse();
+        if (winningHorse != null) {
+            out.println("<h2>우승 말</h2>");
+            out.println("<p>말 이름: " + winningHorse.getName() + "</p>");
+            out.println("<p>유저: " + winningHorse.getUser() + "</p>");
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String horseNamesParam = request.getParameter("horses");
+        String userNamesParam = request.getParameter("users");
+        int targetDistance = Integer.parseInt(request.getParameter("targetDistance"));
+
+        if (isValidParameters(horseNamesParam, userNamesParam)) {
+            String[] horseNames = SplitUtil.split(horseNamesParam);
+            String[] userNames = SplitUtil.split(userNamesParam);
+
+            if (horseNames.length == userNames.length) {
+                Horses horses = createHorses(horseNames, userNames, targetDistance);
+                game.setHorses(horses);
+            }
+        }
+
+        game.startRace();
+    }
+
+    private boolean isValidParameters(String horseNamesParam, String userNamesParam) {
+        return horseNamesParam != null && !horseNamesParam.isEmpty() &&
+                userNamesParam != null && !userNamesParam.isEmpty();
+    }
+
+    private Horses createHorses(String[] horseNames, String[] userNames, int targetDistance) {
+        Horses horses = Horses.create();
+        for (int i = 0; i < horseNames.length; i++) {
+            Horse horse = new Horse(horseNames[i]);
+            horse.setUser(userNames[i]);
+            horse.setTargetDistance(targetDistance);
+            horses.addHorse(horse);
+        }
+
+        return horses;
+    }
+}
--- src/main/webapp/user/game.jsp
+++ src/main/webapp/user/game.jsp
@@ -1,8 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-
 <html>
 <head>
     <title>경마</title>
+    <style>
+        body {
+            background-color: #232736 !important; /* 우선순위를 높이기 위한 구체적인 선택자 */
+        }
+        </style>
 </head>
 <body>
 
